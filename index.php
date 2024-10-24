e0ecdde3a867a4f5220eceacbfce4a8707b9b993
Reihan Khadafi <if23.reihanbindwiyan@mhs.ubpkarawang.ac.id>
2024-10-24 21:28:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ini Halaman Home</title>
+</head>
+<body>
+    <h1>ini halaman login</h1>
+    <form action="login.php" method="post">
+        <input type="email" name="email" id="email" placeholder="email">
+        <input type="password" name="password" id="password" placeholder="password">
+        <button type="submit">Login</button>
+    </form>
+
+
+</body>
+</html>
